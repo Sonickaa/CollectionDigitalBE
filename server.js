990271309082398fd0d4fe2a716cb986da3d57df
Sonia Chaboud <sonia.chaboud@gmail.com>
2023-10-18 15:36:29 +0200

    commented out parts to fetch from api

--- server.js
+++ server.js
@@ -12,9 +12,24 @@ app.use(cors());
 
 connectDB();
 
+/* const API = "https://api.vam.ac.uk/v2"; */
+
 app.get("/", (req, res) => {
   res.send("Welcome to digital collective");
 });
+
+/* app.get("/objects", async (req, res) => {
+  try {
+    const response = await axios.get(API);
+    const data = response.data;
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching data from the API" });
+  }
+}); */
 
 app.listen(PORT, () => {
   console.log(`App listening on port http://localhost:${PORT}`.rainbow);
