e69257850b2bc580308e3ec8a0321fa9c732601a
Jennifer Rothrock <121233385+jeniverede@users.noreply.github.com>
2023-11-02 09:43:30 +0100

    Cloudinary for user collection images

---
+++ controllers/imageController.js
@@ -0,0 +1,37 @@
+const Image = require("../schemas/Image");
+
+const getImages = async (req, res) => {
+    try {
+        const images = await Image.find();
+        if (!images.length) {
+            return res.status(200).json({ msg: "No images found" });
+        } else {
+            return res.status(200).json(images);
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json(error);
+    }
+};
+
+const uploadImage = async (req, res) => {
+    try {
+        if (req.file && req.file.path) {
+            const image = new Image({
+                url: req.file.path,
+                description: req.body.desc,
+            });
+
+            await image.save();
+            return res.status(200).json({ msg: "Image saved successfully" });
+        } else {
+            console.log(req.file);
+            res.status(422).json({ msg: "Image format is invalid" });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json(error);
+    }
+};
+
+module.exports = { getImages, uploadImage };
